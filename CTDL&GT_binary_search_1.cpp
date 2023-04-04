29bdeb7f50c3f1cd4fa4706becfef8ccf3dae658
Thog512 <115013206+Thog512@users.noreply.github.com>
2023-04-04 15:21:49 +0700

    Update CTDL&GT_binary_search_1.cpp

--- CTDL&GT_binary_search_1.cpp
+++ CTDL&GT_binary_search_1.cpp
@@ -1,17 +1,17 @@
 \\nguon: https://codelearn.io/learning/data-structure-and-algorithms/825257
 
 int BinarySearch(int a[], int n, int x){
-	int l = 0, r = n-1;
+	int l = 0, r = n-1; //gia tri ban dau cua left va right
 	while (l < r){
-		int mid = (l+r)/2;
-		if (a[mid] < x){
+		int mid = (l+r)/2; // cong thuc tinh mid(phan tu o giua mang)
+		if (a[mid] < x){ //neu phan tu mid < x thi ta cap nhat left = mid tang 1 don vi (mid +1)
 			l = mid + 1;
 		}
-		else{
+		else{ //nguoc lai neu phan tu mid > x thi ta cap nhat right = mid
 			r = mid;
 		}
 	}
-	if (a[l] == x){
+	if (a[l] == x){ //kiểm tra a[l] nếu a[l] bằng x thì đưa ra l, ngược lại đưa ra -1 
 		return l;
 	}
 	return -1;
