a806ff6eb2b76816873f415d4aac98e7ceaa8ca5
Thog512 <115013206+Thog512@users.noreply.github.com>
2023-04-04 17:08:24 +0700

    Update CTDL&GT_Insertion_Sort_1.cpp

--- CTDL&GT_Insertion_Sort_1.cpp
+++ CTDL&GT_Insertion_Sort_1.cpp
@@ -1,14 +1,14 @@
 //nguon: https://codelearn.io/sharing/cac-thuat-toan-sap-xep-trong-cpp
 
 void InsertionSort(int a[], int n){
-	int i, j, t;
+	int i, j, x;
 	for (i=1; i<n; i++){
-		t = a[i];
+		x = a[i];
 		j = i-1;
-		while(j >= 0 && a[j] > t){
+		while(j >= 0 && a[j] > x){
 			a[j+1] = a[j];
 			j= j-1;;
 		}
-		a[j+1] = t; 
+		a[j+1] = x; 
 	}
 }
